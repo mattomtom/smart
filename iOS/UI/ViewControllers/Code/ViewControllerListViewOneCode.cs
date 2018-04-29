d07567937a29b26899653acf49ac8c6f7ca6859a
Mateusz Tomaszewski <>
2018-04-29 17:21:12 +0200

    + add menu table with swap buttons

---
+++ iOS/UI/ViewControllers/Code/ViewControllerListViewOneCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UIKit;
+
+namespace Smart.iOS.UI.ViewControllers.Code
+{
+    public class ViewControllerListViewOneCode : UIViewController
+    {
+        public ViewControllerListViewOneCode()
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            NavigationItem.Title = "List View One";
+            View.BackgroundColor = UIColor.DarkGray;
+        }
+    }
+}
--- iOS/UI/ViewControllers/ViewControllerHome.cs
+++ iOS/UI/ViewControllers/ViewControllerHome.cs
@@ -2,34 +2,120 @@
 using System.Collections.Generic;
 using UIKit;
 
-using Smart.iOS.DataSource;
+using Foundation;
+using Smart.iOS.UI.ViewControllers.Code;
 
 namespace Smart.iOS.UI.ViewControllers
 {
-    public class ViewControllerHome : UIViewController
+    public class NavViewItem
     {
-        IList<string> _menuItems;
+        public string Title { get; set; }
+        public string Descrition { get; set; }
+        public UIViewController ViewControllerCode { get; set; }
+        public UIViewController ViewControllerXib { get; set; }
+    }
+    
+    public class ViewControllerHome : UITableViewController
+    {
+        IList<NavViewItem> _menuItems;
 
         public ViewControllerHome()
         {
-            _menuItems = new List<string>
-            {
-                "Test1",
-                "Test2",
-                "Test3"
-            };
+            SetupViewControllers();
         }
 
 		public override void ViewDidLoad()
 		{
             base.ViewDidLoad();
 
-            NavigationController.SetNavigationBarHidden(hidden: true, animated: false);
-
-            UITableView tableView = new UITableView(frame: UIScreen.MainScreen.Bounds);
-            View.AddSubview(tableView);
-
-            tableView.Source = new TableViewHomeSource(_menuItems);
+            NavigationItem.Title = "Home";
 		}
+
+		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            UITableViewCell cell = tableView.DequeueReusableCell("cellInHome");
+
+            if (cell == null)
+            {
+                cell = new UITableViewCell(UITableViewCellStyle.Default, "cellInHome");
+            }
+            cell.TextLabel.Text = _menuItems[indexPath.Row].Title;
+
+            return cell;
+        }
+
+
+		public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return _menuItems.Count;
+        }
+
+        public override UIView GetViewForFooter(UITableView tableView, nint section)
+        {
+            // clear empty cells
+            return new UIView();
+        }
+
+		public override UITableViewCellEditingStyle EditingStyleForRow(UITableView tableView, NSIndexPath indexPath)
+		{
+            // hide delete action
+            return UITableViewCellEditingStyle.None;
+		}
+
+		public override UISwipeActionsConfiguration GetLeadingSwipeActionsConfiguration(UITableView tableView, NSIndexPath indexPath)
+        {
+            UIContextualAction codeAction = ContextualCodeAction(indexPath.Row);
+            UIContextualAction xibAction = ContextualXibAction(indexPath.Row);
+            UISwipeActionsConfiguration leadingSwipe = UISwipeActionsConfiguration
+                .FromActions(new UIContextualAction[] { codeAction, xibAction });
+
+            leadingSwipe.PerformsFirstActionWithFullSwipe = false;
+
+            return leadingSwipe;
+        }
+
+        private UIContextualAction ContextualCodeAction(int row)
+        {
+            UIContextualAction action = UIContextualAction.FromContextualActionStyle(
+                UIContextualActionStyle.Normal,
+                "Code",
+                (ReadLaterAction, view, success) => {
+                    NavigationController.PushViewController(_menuItems[row].ViewControllerCode, false);
+                });
+
+            action.BackgroundColor = UIColor.Orange;
+
+            return action;
+        }
+
+        private UIContextualAction ContextualXibAction(int row)
+        {
+            UIContextualAction action = UIContextualAction.FromContextualActionStyle(
+                UIContextualActionStyle.Normal,
+                "XIB",
+                (ReadLaterAction, view, success) => {
+                    NavigationController.PushViewController(_menuItems[row].ViewControllerXib, false);
+
+                    success(true);
+                });
+
+            action.BackgroundColor = UIColor.Blue;
+
+            return action;
+        }
+
+        private void SetupViewControllers()
+        {
+            _menuItems = new List<NavViewItem>
+            {
+                new NavViewItem
+                {
+                    Title = "List View One",
+                    Descrition = "List View description",
+                    ViewControllerCode = new ViewControllerListViewOneCode(),
+                    ViewControllerXib = new ViewControllerListViewOneCode()
+                }
+            };
+        }
 	}
 }
