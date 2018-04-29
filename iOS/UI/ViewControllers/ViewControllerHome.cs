4e5b9e3b00c0bf17bc2bb491eb7d500587edb0e0
Mateusz Tomaszewski <>
2018-04-29 12:10:52 +0200

    + Add Table View to Home screen

--- iOS/UI/ViewControllers/ViewControllerHome.cs
+++ iOS/UI/ViewControllers/ViewControllerHome.cs
@@ -1,19 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 using UIKit;
+
+using Smart.iOS.DataSource;
 
 namespace Smart.iOS.UI.ViewControllers
 {
     public class ViewControllerHome : UIViewController
     {
+        IList<string> _menuItems;
+
         public ViewControllerHome()
         {
+            _menuItems = new List<string>
+            {
+                "Test1",
+                "Test2",
+                "Test3"
+            };
         }
 
 		public override void ViewDidLoad()
 		{
             base.ViewDidLoad();
 
-            View.BackgroundColor = UIColor.DarkGray;
+            NavigationController.SetNavigationBarHidden(hidden: true, animated: false);
+
+            UITableView tableView = new UITableView(frame: UIScreen.MainScreen.Bounds);
+            View.AddSubview(tableView);
+
+            tableView.Source = new TableViewHomeSource(_menuItems);
 		}
 	}
 }
